d8e31dd4242472e9bdd935d2608b2f14afeefcfd
sidofdountio <sidofdountio406@gmail.com>
2024-04-30 16:18:58 +0100

    configure prod env

--- start-dev.sh
+++ start-dev.sh
@@ -1,1 +1,4 @@
-ENV_FILE=./.env.dev docker compose  up -d --build
+# This Docker cmd start service and build App image from Dockerfile
+#ENV_FILE=./.env.dev docker compose  up -d --build
+# This docker cmd start docker services
+ENV_FILE=./.env.dev docker compose  up -d
