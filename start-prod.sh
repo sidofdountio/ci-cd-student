7408ee470896cb70d1516a9c746b19ab7cca312e
sidofdountio <sidofdountio406@gmail.com>
2024-05-17 09:39:09 +0100

    port config 7

--- start-prod.sh
+++ start-prod.sh
@@ -2,3 +2,5 @@
 ENV_FILE=./.env.prod docker compose  up -d --build
 # This docker cmd start docker services
 #ENV_FILE=./.env.prod docker compose  up -d
+#docker tag devops-skills-api:latest dountio/devops-skills-api:latest
+#docker push dountio/devops-skills-api:latest
