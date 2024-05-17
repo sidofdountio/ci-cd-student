c0561e123245dcf2becace28a31eebb5c8dc2e79
sidofdountio <sidofdountio406@gmail.com>
2024-05-17 03:51:56 +0100

    port config

--- start-prod.sh
+++ start-prod.sh
@@ -1,4 +1,4 @@
 # This Docker cmd start service and build App image from Dockerfile
-ENV_FILE=./.env.dev docker compose  up -d --build
+ENV_FILE=./.env.prod docker compose  up -d --build
 # This docker cmd start docker services
-#ENV_FILE=./.env.dev docker compose  up -d
+#ENV_FILE=./.env.prod docker compose  up -d
